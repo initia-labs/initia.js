c06f73d2df770145d35217c3a6d562ee8a17bbc6
joon9823 <joon9823@gmail.com>
2023-11-24 12:33:18 +0900

    Fix bug on wrong validator for opchild api

--- src/client/lcd/api/OpchildAPI.ts
+++ src/client/lcd/api/OpchildAPI.ts
@@ -1,29 +1,29 @@
 import { BaseAPI } from './BaseAPI';
 import { APIParams, Pagination, PaginationOptions } from '../APIRequester';
-import { Validator, ValAddress, OpchildParams } from '../../../core';
+import { OpValidator, ValAddress, OpchildParams } from '../../../core';
 
 export class OpchildAPI extends BaseAPI {
   public async validators(
     params: Partial<PaginationOptions & APIParams> = {}
-  ): Promise<[Validator[], Pagination]> {
+  ): Promise<[OpValidator[], Pagination]> {
     return this.c
       .get<{
-        validators: Validator.Data[];
+        validators: OpValidator.Data[];
         pagination: Pagination;
       }>(`/opinit/opchild/v1/validators`, params)
-      .then(d => [d.validators.map(Validator.fromData), d.pagination]);
+      .then(d => [d.validators.map(OpValidator.fromData), d.pagination]);
   }
 
   public async validator(
     validatorAddr: ValAddress,
     params: APIParams = {}
-  ): Promise<Validator> {
+  ): Promise<OpValidator> {
     return this.c
-      .get<{ validator: Validator.Data }>(
+      .get<{ validator: OpValidator.Data }>(
         `/opinit/opchild/v1/validator/${validatorAddr}`,
         params
       )
-      .then(d => Validator.fromData(d.validator));
+      .then(d => OpValidator.fromData(d.validator));
   }
 
   public async parameters(params: APIParams = {}): Promise<OpchildParams> {
