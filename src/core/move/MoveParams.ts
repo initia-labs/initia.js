bca9299528d9ea93f829722067989fe8cadd88ad
joon9823 <joon9823@gmail.com>
2023-12-14 13:15:13 +0900

    Update move params

--- src/core/move/MoveParams.ts
+++ src/core/move/MoveParams.ts
@@ -1,6 +1,5 @@
 import { JSONSerializable } from '../../util/json';
 import { Params as Params_pb } from '@initia/initia.proto/initia/move/v1/types';
-import Long from 'long';
 
 export class MoveParams extends JSONSerializable<
   MoveParams.Amino,
@@ -8,18 +7,18 @@ export class MoveParams extends JSONSerializable<
   MoveParams.Proto
 > {
   /**
-   * @param max_module_size
    * @param base_denom
    * @param base_min_gas_price
    * @param arbitrary_enabled
    * @param contract_shared_revenue_ratio the percentage of fees distributed to developers
+   * @param allowed_publishers list of addresses with permission to distribute contracts, empty list is interpreted as allowing anyone to distribute
    */
   constructor(
-    public max_module_size: number,
     public base_denom: string,
     public base_min_gas_price: string,
     public arbitrary_enabled: boolean,
-    public contract_shared_revenue_ratio: string
+    public contract_shared_revenue_ratio: string,
+    public allowed_publishers: string[]
   ) {
     super();
   }
@@ -27,101 +26,101 @@ export class MoveParams extends JSONSerializable<
   public static fromAmino(data: MoveParams.Amino): MoveParams {
     const {
       value: {
-        max_module_size,
         base_denom,
         base_min_gas_price,
         arbitrary_enabled,
         contract_shared_revenue_ratio,
+        allowed_publishers,
       },
     } = data;
     return new MoveParams(
-      Number.parseInt(max_module_size),
       base_denom,
       base_min_gas_price,
       arbitrary_enabled,
-      contract_shared_revenue_ratio
+      contract_shared_revenue_ratio,
+      allowed_publishers
     );
   }
 
   public toAmino(): MoveParams.Amino {
     const {
-      max_module_size,
       base_denom,
       base_min_gas_price,
       arbitrary_enabled,
       contract_shared_revenue_ratio,
+      allowed_publishers,
     } = this;
     return {
       type: 'move/Params',
       value: {
-        max_module_size: max_module_size.toString(),
         base_denom,
         base_min_gas_price,
         arbitrary_enabled,
         contract_shared_revenue_ratio,
+        allowed_publishers,
       },
     };
   }
 
   public static fromData(data: MoveParams.Data): MoveParams {
     const {
-      max_module_size,
       base_denom,
       base_min_gas_price,
       arbitrary_enabled,
       contract_shared_revenue_ratio,
+      allowed_publishers,
     } = data;
     return new MoveParams(
-      Number.parseInt(max_module_size),
       base_denom,
       base_min_gas_price,
       arbitrary_enabled,
-      contract_shared_revenue_ratio
+      contract_shared_revenue_ratio,
+      allowed_publishers
     );
   }
 
   public toData(): MoveParams.Data {
     const {
-      max_module_size,
       base_denom,
       base_min_gas_price,
       arbitrary_enabled,
       contract_shared_revenue_ratio,
+      allowed_publishers,
     } = this;
     return {
       '@type': '/initia.move.v1.Params',
-      max_module_size: max_module_size.toString(),
       base_denom,
       base_min_gas_price,
       arbitrary_enabled,
       contract_shared_revenue_ratio,
+      allowed_publishers,
     };
   }
 
   public static fromProto(data: MoveParams.Proto): MoveParams {
     return new MoveParams(
-      data.maxModuleSize.toNumber(),
       data.baseDenom,
       data.baseMinGasPrice,
       data.arbitraryEnabled,
-      data.contractSharedRevenueRatio
+      data.contractSharedRevenueRatio,
+      data.allowedPublishers
     );
   }
 
   public toProto(): MoveParams.Proto {
     const {
-      max_module_size,
       base_denom,
       base_min_gas_price,
       arbitrary_enabled,
       contract_shared_revenue_ratio,
+      allowed_publishers,
     } = this;
     return Params_pb.fromPartial({
-      maxModuleSize: Long.fromNumber(max_module_size),
       baseDenom: base_denom,
       baseMinGasPrice: base_min_gas_price,
       arbitraryEnabled: arbitrary_enabled,
       contractSharedRevenueRatio: contract_shared_revenue_ratio,
+      allowedPublishers: allowed_publishers,
     });
   }
 }
@@ -130,21 +129,21 @@ export namespace MoveParams {
   export interface Amino {
     type: 'move/Params';
     value: {
-      max_module_size: string;
       base_denom: string;
       base_min_gas_price: string;
       arbitrary_enabled: boolean;
       contract_shared_revenue_ratio: string;
+      allowed_publishers: string[];
     };
   }
 
   export interface Data {
     '@type': '/initia.move.v1.Params';
-    max_module_size: string;
     base_denom: string;
     base_min_gas_price: string;
     arbitrary_enabled: boolean;
     contract_shared_revenue_ratio: string;
+    allowed_publishers: string[];
   }
 
   export type Proto = Params_pb;
