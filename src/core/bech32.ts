3d2210018ea5882fe5cf1e24cda1c604552e6914
ALPAC-4 <contact.alpac4@gmail.com>
2024-02-21 15:51:31 +0900

    fix: remove 0x

--- src/core/bech32.ts
+++ src/core/bech32.ts
@@ -67,7 +67,7 @@ export namespace AccAddress {
    * @param hexAddress hex address
    */
   export function fromHex(hexAddress: string): AccAddress {
-    const hex = hexAddress.replace(/^0x0+|^0+(?!x)/, '0x');
+    const hex = hexAddress.replace(/^0x0+|^0+(?!x)/, '');
     // That moudule address reach here is nearly impossible
     if (hex.length <= 40) {
       return bech32.encode(
