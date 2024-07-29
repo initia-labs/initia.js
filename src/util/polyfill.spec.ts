6c65e064e65b0adef4cd427c705c103d2dc15df5
Vritra4 <Vritra@hitel.net>
2024-07-29 16:27:42 +0900

    add polyfill

---
+++ src/util/polyfill.spec.ts
@@ -0,0 +1,41 @@
+import { bytesFromBase64, base64FromBytes } from './polyfill';
+
+describe('bytesFromBase64', () => {
+  const b64Data = 'SGVsbG8gd29ybGQ=';
+  const arrData = new Uint8Array([
+    72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100,
+  ]);
+
+  it('should convert base64 string to Uint8Array', () => {
+    const result = bytesFromBase64(b64Data);
+
+    expect(result).toEqual(arrData);
+  });
+
+  // Handles empty base64 strings
+  it('should return an empty Uint8Array when given an empty base64 string', () => {
+    const b64 = '';
+    const expected = new Uint8Array(0);
+
+    const result = bytesFromBase64(b64);
+
+    expect(result).toEqual(expected);
+  });
+
+  // Converts a base64 string to a Uint8Array using Buffer when available
+  it('should convert Uint8Array to base64 string', () => {
+    const result = base64FromBytes(arrData);
+
+    expect(result).toEqual(b64Data);
+  });
+
+  // Handles empty base64 strings
+  it('should return an empty string when given an empty Uint8Array', () => {
+    const emptyArr = new Uint8Array(0);
+    const expected = '';
+
+    const result = base64FromBytes(emptyArr);
+
+    expect(result).toEqual(expected);
+  });
+});
---
+++ src/util/polyfill.ts
@@ -0,0 +1,57 @@
+declare const self: any | undefined;
+declare const window: any | undefined;
+declare const global: any | undefined;
+
+const gt: any = (() => {
+  if (typeof globalThis !== 'undefined') {
+    return globalThis;
+  }
+  if (typeof self !== 'undefined') {
+    return self;
+  }
+  if (typeof window !== 'undefined') {
+    return window;
+  }
+  if (typeof global !== 'undefined') {
+    return global;
+  }
+  throw 'Unable to locate global object';
+})();
+
+/**
+ * Converts a base64 string to Uint8Array.
+ *
+ * @param b64 - The base64 string to convert.
+ * @returns The Uint8Array representation of the base64 string.
+ */
+export function bytesFromBase64(b64: string): Uint8Array {
+  if ((gt as any).Buffer) {
+    return Uint8Array.from(gt.Buffer.from(b64, 'base64'));
+  } else {
+    const bin = gt.atob(b64);
+    const arr = new Uint8Array(bin.length);
+    for (let i = 0; i < bin.length; ++i) {
+      arr[i] = bin.charCodeAt(i);
+    }
+    return arr;
+  }
+}
+
+/**
+ * Converts a Uint8Array to a base64 encoded string.
+ * Uses 'Buffer' if available in the global object, otherwise performs the conversion manually.
+ *
+ * @param arr - The Uint8Array to be converted to base64.
+ * @returns The base64 encoded string representation of the input Uint8Array.
+ */
+export function base64FromBytes(arr: Uint8Array): string {
+  if ((gt as any).Buffer) {
+    return gt.Buffer.from(arr).toString('base64');
+  } else {
+    const bin: string[] = [];
+    arr.forEach(byte => {
+      bin.push(gt.String.fromCharCode(byte));
+    });
+    return gt.btoa(bin.join(''));
+  }
+}
