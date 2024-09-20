a57ca6f0928692feaf39c6cac926aff530ce99f0
joon9823 <joon9823@gmail.com>
2024-09-20 15:46:29 +0900

    Add cjs to vite config and add exports in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      fileName: () => 'index.js',
-      formats: ['es'],
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'cjs'],
     },
     rollupOptions: {
       external: [
