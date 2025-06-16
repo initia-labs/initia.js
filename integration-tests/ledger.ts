be94a9cf60b84c3c410909ec4e1dad4b935c009e
Geoff Lee <Vritra@hitel.net>
2025-06-16 16:31:21 +0900

    chore fix

--- integration-tests/ledger.ts
+++ integration-tests/ledger.ts
@@ -1,28 +1,39 @@
-//const TransportNodeHid = require("@ledgerhq/hw-transport-node-hid").default;
 import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
-import { RESTClient, LedgerKey, MsgSend, MsgExecute, Wallet, SignMode, bcs, Kind, Fee, Coins} from '../src'
-
-//testImports().catch(console.error);
+import {
+  RESTClient,
+  LedgerKey,
+  MsgSend,
+  MsgExecute,
+  Wallet,
+  SignMode,
+  bcs,
+  Kind,
+  Fee,
+  Coins,
+} from '../src'
 
 async function main() {
-  console.log("=== Starting Main Function ===");
+  const appKind = Kind.Ethereum
 
-  const isEth = false
+  const transport = await TransportNodeHid.create()
+  const ledgerKey =
+    appKind === Kind.Ethereum
+      ? await LedgerKey.createEthereumApp(transport)
+      : await LedgerKey.createCosmosApp(transport)
 
-  const t = await TransportNodeHid.create()
-  const ledgerKey = isEth?(await LedgerKey.createEthereumApp(t)):(await LedgerKey.createCosmosApp(t));
+  console.log(
+    `Address: ${await ledgerKey.getApplication().getAddress(ledgerKey.getPath(), false)}`
+  )
 
-  console.log("accAddress:", ledgerKey.accAddress)
-  console.log("appConfiguration:", await ledgerKey.getAppConfiguration())
+  console.log('accAddress:', ledgerKey.accAddress)
+  console.log('appConfiguration:', await ledgerKey.getAppConfiguration())
 
-  const restClient = new RESTClient("https://rest.testnet.initia.xyz");
-  const wallet = new Wallet(restClient, ledgerKey);
+  const restClient = new RESTClient('https://rest.testnet.initia.xyz')
+  const wallet = new Wallet(restClient, ledgerKey)
 
-  const msg1 = new MsgSend(
-    ledgerKey.accAddress,
-    ledgerKey.accAddress,
-    { uinit: "1" }
-  );
+  const msg1 = new MsgSend(ledgerKey.accAddress, ledgerKey.accAddress, {
+    uinit: '1',
+  })
 
   const msg2 = new MsgExecute(
     ledgerKey.accAddress,
@@ -31,8 +42,18 @@ async function main() {
     'swap_script',
     [],
     [
-      bcs.object().serialize('0xdbf06c48af3984ec6d9ae8a9aa7dbb0bb1e784aa9b8c4a5681af660cf8558d7d').toBase64(),
-      bcs.object().serialize('0x8e4733bdabcf7d4afc3d14f0dd46c9bf52fb0fce9e4b996c939e195b8bc891d9').toBase64(),
+      bcs
+        .object()
+        .serialize(
+          '0xdbf06c48af3984ec6d9ae8a9aa7dbb0bb1e784aa9b8c4a5681af660cf8558d7d'
+        )
+        .toBase64(),
+      bcs
+        .object()
+        .serialize(
+          '0x8e4733bdabcf7d4afc3d14f0dd46c9bf52fb0fce9e4b996c939e195b8bc891d9'
+        )
+        .toBase64(),
       bcs.u64().serialize(1000000).toBase64(),
       bcs.option(bcs.u64()).serialize(null).toBase64(),
     ]
@@ -41,23 +62,24 @@ async function main() {
   const fee = new Fee(1000000, Coins.fromString('300000uinit'))
 
   const textObj = {
-    Title: "title",
-    Content: "content",
+    Title: 'title',
+    Content: 'content',
     Indent: 2,
-    Expert: false
+    Expert: false,
   }
 
   const tx = await wallet.createAndSignTx({
     msgs: [msg1, msg2],
     fee,
-    signMode: isEth?SignMode.SIGN_MODE_EIP_191:SignMode.SIGN_MODE_LEGACY_AMINO_JSON
+    signMode:
+      appKind === Kind.Ethereum
+        ? SignMode.SIGN_MODE_EIP_191
+        : SignMode.SIGN_MODE_LEGACY_AMINO_JSON,
   })
-  console.log("Transaction signed:", JSON.stringify(tx.toData()));
+  console.log('Transaction signed:', JSON.stringify(tx.toData()))
 
-  const result = await restClient.tx.broadcastSync(tx);
-  console.log("Transaction broadcast result:", result);
-
-  console.log("=== Main Function Completed ===");
+  const result = await restClient.tx.broadcastSync(tx)
+  console.log('Transaction broadcast result:', result)
 }
 
-main().catch(console.error);
+main().catch(console.error)
